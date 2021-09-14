31dc4f0cb9c9eba77e551af332a80c95a550ff61
ig-cardoso <iygorsilva@gmail.com>
2021-09-13 22:57:22 -0300

    .

--- SAPRON-backend/userCalendar/views.py
+++ SAPRON-backend/userCalendar/views.py
@@ -8,17 +8,11 @@ from userCalendar.models import Controle, Checkin, Checkout, Limpeza
 # Create your views here.
 
 
-# tasks = Task.objects.filter(title__icontains=search, user=request.user)
-
 def calendar(request):
     controles = Controle.objects.all() #.order_by('')
     checkins = Checkin.objects.all().order_by('data_checkin') #.order_by('hora_checkin')
     checkouts = Checkout.objects.all().order_by('data_checkout') #.order_by('hora_checkout')
     limpezas = Limpeza.objects.all().order_by('data_limpeza') #.order_by('hora_limpeza')
-
-    # paginacao = Paginator(controles, 1)
-    # page = request.GET.get('page')	
-    # controles = paginacao.get_page(page) 
 
 
     return render(request, 'controle.html', {'controles': controles,
