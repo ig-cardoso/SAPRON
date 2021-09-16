7d8bedc533ff48fbd227a72547f39d283bdc9fae
ig-cardoso <iygorsilva@gmail.com>
2021-09-15 23:09:44 -0300

    .

--- SAPRON-backend/userCalendar/models.py
+++ SAPRON-backend/userCalendar/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import manager
 
+from djmoney.models.fields import MoneyField
 # Create your models here.
 
 
@@ -24,13 +25,24 @@ class Locacao(models.Model):
         db_table = 'Locacao'
         verbose_name_plural = u'Locacoes' 
 
-
 class Checkin(models.Model):
-    id_checkin = models.AutoField(primary_key=True)
-    hora_checkin = models.TimeField(auto_now=False, auto_now_add=False, blank=False, null=True)
+    # id_checkin = models.AutoField(primary_key=True)
+    id_checkin = models.IntegerField(primary_key=True, blank=False)
+    codigo_reserva = models.CharField(max_length=10)
     data_checkin = models.DateField(auto_now=False, auto_now_add=False, blank=False)
-    responsavel_checkin = models.CharField(max_length=100)
-    status_checkin = models.CharField(max_length=7, choices=STATUS)
+    hora_checkin = models.TimeField(auto_now=False, auto_now_add=False, blank=False, null=True)
+    endereco = models.CharField(max_length=100)
+    cep = models.CharField(max_length=9)
+    nome_usuario = models.CharField(max_length=50)
+    numero_usuario = models.CharField(max_length=25)
+    quatidade_adultos = models.IntegerField(blank=False)
+    quatidade_criancas = models.IntegerField(blank=False)
+    quantidade_pet = models.IntegerField(blank=False)
+    quantidade_diarias = models.IntegerField(blank=False)
+    valor_a_pagar = MoneyField(decimal_places=0, default=0.0, default_currency='USD', max_digits=11,)
+    ja_pago = models.BooleanField(null=False, blank=False)
+
+    status = models.CharField(max_length=7, choices=STATUS)
 
     def __str__(self):
         return str(self.id_checkin)
@@ -40,13 +52,19 @@ class Checkin(models.Model):
         db_table = 'CheckIn'
         verbose_name_plural = u'Checkins' 
 
-
 class Checkout(models.Model):
-    id_checkout = models.AutoField(primary_key=True)
-    hora_checkout = models.TimeField(auto_now=False, auto_now_add=False, blank=False, null=True)
-    data_checkout = models.DateField(auto_now=False, auto_now_add=False, blank=False)
-    responsavel_checkout = models.CharField(max_length=100)
-    status_checkout = models.CharField(max_length=7, choices=STATUS)
+    id_checkout = models.IntegerField(primary_key=True, blank=False)
+    codigo_reserva = models.CharField(max_length=10)
+    data_checkou = models.DateField(auto_now=False, auto_now_add=False, blank=False)
+    hora_checkou = models.TimeField(auto_now=False, auto_now_add=False, blank=False, null=True)
+    endereco = models.CharField(max_length=100)
+    cep = models.CharField(max_length=9)
+    nome_usuario = models.CharField(max_length=50)
+    numero_usuario = models.CharField(max_length=25)
+    quantidade_diarias = models.IntegerField(blank=False)
+    valor_a_pagar = MoneyField(decimal_places=0, default=0, default_currency='USD', max_digits=11,)
+    ja_pago = models.BooleanField(null=False, blank=False)
+    status = models.CharField(max_length=7, choices=STATUS)
 
     def __str__(self):
         return str(self.id_checkout)
@@ -56,10 +74,9 @@ class Checkout(models.Model):
         db_table = 'Checkout'
         verbose_name_plural = u'Checkouts' 
 
-
 class Limpeza(models.Model):
     # id_limpeza = models.AutoField(primary_key=True)
-    id_limpeza = models.IntegerField(primary_key=True, blank=False,)
+    id_limpeza = models.IntegerField(primary_key=True, blank=False)
     hora_limpeza = models.TimeField(auto_now=False, auto_now_add=False, blank=False, null=True)
     data_limpeza = models.DateField(auto_now=False, auto_now_add=False, blank=False)
     status_limpeza = models.CharField(max_length=7, choices=STATUS)
--- SAPRON-backend/userCalendar/views.py
+++ SAPRON-backend/userCalendar/views.py
@@ -1,6 +1,5 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.core.paginator import Paginator 
 
 from userCalendar.models import Locacao, Checkin, Checkout, Limpeza
@@ -11,25 +10,46 @@ import json
 # Create your views here.
 
 
+def calendar_(request):
+    # Opetei por não gurdar os dados no banco, pois os dados são facilmente mutaveis
+    # Pensei que o anfitreão poderá ter imóveis cadatro, então o Airbnb é quem manda o endereço no checkin
+    user_id = 1
+    airbnb_request = requests.get("http://localhost:3001/usuario/"+str(user_id)) # Requisição a API fake do Airbnb
+    airbnb_dados = json.loads(airbnb_request.content)
+    
+    limpezas = Limpeza.objects.all().order_by('data_limpeza')
+    
+
+
+    return render(request, 'controle.html', {'checkin_user': airbnb_dados['checkin'],
+                                            'checkout_user': airbnb_dados['checkout'],
+                                            'limpezas': limpezas})
+    # return airbnb_dados
+
+
+
+import pprint
+
 def calendar(request):
+    airbnb_request = requests.get("http://localhost:3001/teste")
+    airbnb_dados = json.loads(airbnb_request.content)
+    
+    # Add ou atulza o dado de limpeza no banco de daos
+
+    
+    pprint.pprint(airbnb_dados)
+
+    for dado_airbnb in airbnb_dados:
+        
+        teste = Limpeza.objects.update_or_create(hora_limpeza=dado_airbnb['hora_limpeza'],
+                                                data_limpeza=dado_airbnb['data_limpeza'],
+                                                status_limpeza=dado_airbnb['status_limpeza'],
+                                                id_limpeza=dado_airbnb['id'])
+       
+
     locacao = Locacao.objects.all() #.order_by('')
     checkins = Checkin.objects.all().order_by('data_checkin') #.order_by('hora_checkin')
     checkouts = Checkout.objects.all().order_by('data_checkout') #.order_by('hora_checkout')
     limpezas = Limpeza.objects.all().order_by('data_limpeza') #.order_by('hora_limpeza')
 
-    airbnb_request = requests.get("http://localhost:3001/teste")
-    airbnb_dados = json.loads(airbnb_request.content)
-    
-
-    # Add ou atulza o dado de limpeza no banco de daos
-    for dado_airbnb in airbnb_dados:
-        teste = Limpeza.objects.update_or_create(hora_limpeza=dado_airbnb['hora_limpeza'],
-                                                data_limpeza=dado_airbnb['data_limpeza'],
-                                                status_limpeza=dado_airbnb['status_limpeza'],
-                                                id_limpeza=dado_airbnb['id'])
-    
-
-    return render(request, 'controle.html', {'locacao': locacao,
-                                            'checkins': checkins,
-                                            'checkouts': checkouts,
-                                            'limpezas': limpezas})
+    return teste
